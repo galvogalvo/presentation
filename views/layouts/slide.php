0cbded4be01b4803be03d58773929777e6641dcc
Pedro Duarte <pedro.duarte@rga.com>
2012-12-13 15:19:46 +0000

    Added video functionality

--- views/layouts/slide.php
+++ views/layouts/slide.php
@@ -44,6 +44,7 @@
 					</header>
 					<p>This <code>&lt;tool&gt;</code> is provided <strong>without</strong> warranty, guarantee, or much in the way of explanation. Note that use of this tool may or may not crash <em>your</em> browser.</p>
 					<p><a href="examples/index.htm" target="_blank">This link will be opened in the new tab</a></p>
+					<!-- <iframe src="http://www.youtube.com/embed/DvAlZMbqhsw" frameborder="0" allowfullscreen></iframe> -->
 				</section>
 			</div>
 		</div>
