77008c0fe1965dd3e51683abd7b4471478b98fd7
Pedro Duarte <pedro.duarte@rga.com>
2012-12-13 14:09:09 +0000

    Added slide layout

---
+++ views/layouts/slide.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en_US"> <!--<![endif]-->
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=1274, user-scalable=no">
+
+
+	<title>R/GA Make Day 2012 - A Better Way To Present</title>
+
+	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+	<link rel="stylesheet" media="screen" href="css/all.css">
+</head>
+
+<body class="full">
+
+	<section id="slideshow" class="slideshow">
+
+		<span class="logo"><i></i>R/GA</span>
+
+		<div class="slide cover" id="Cover">
+			<div>
+				<section>
+					<header>
+						<h1>Make Day <span class="highlight">/</span></h1>
+						<h2>TAKE 2</h2>
+					</header>
+					<p><strong>A Better Way To Present.</strong></p>
+					<p>Halloumipsum dolor sit amet, consectetur adipiscing elit. Middle East rhoncus urna, at semper turpis egestas et. Grilled scelerisque mollis ipsum, id gravida orci viverra sed. Sed sodales dictum ultrices. Etiam nec erat sed ante elementum accumsan nec sit amet nulla.</p>
+					<!-- <img src="img/1.jpg" alt=""> -->
+				</section>
+			</div>
+		</div>
+
+		<div class="slide" id="Header">
+			<div>
+				<section>
+					<header>
+						<h1>Header</h1>
+					</header>
+					<p>This <code>&lt;tool&gt;</code> is provided <strong>without</strong> warranty, guarantee, or much in the way of explanation. Note that use of this tool may or may not crash <em>your</em> browser.</p>
+					<p><a href="examples/index.htm" target="_blank">This link will be opened in the new tab</a></p>
+				</section>
+			</div>
+		</div>
+
+		<div class="slide" id="TwoLinesHeader">
+			<div>
+				<section>
+					<header>
+						<h1>Two rows.<br>Mighty heading</h1>
+					</header>
+					<p>This <code>&lt;tool&gt;</code> is provided <strong>without</strong> warranty, guarantee, or much in the way of explanation. Note that use of this tool may or may not crash <em>your</em> browser.</p>
+				</section>
+			</div>
+		</div>
+
+		<div class="slide" id="ThankYou">
+			<div>
+				<section>
+					<header>
+						<h1>Shower Presentation Template</h1>
+					</header>
+					<p>Vadim Makeev, Opera Software</p>
+					<ul>
+						<li><a href="http://pepelsbey.net">pepelsbey.net</a></li>
+						<li><a href="http://twitter.com/pepelsbey">twitter.com/pepelsbey</a></li>
+						<li><a href="mailto:pepelsbey@gmail.com">pepelsbey@gmail.com</a></li>
+					</ul>
+					<p>Shower: <a href="http://github.com/pepelsbey/shower">github.com/pepelsbey/shower</a></p>
+				</section>
+			</div>
+		</div>
+
+	</section> <!-- /. #slideshow -->
+
+
+	<script src="http://js.pusher.com/1.12/pusher.min.js"></script>
+	<script src="js/libs/jquery-1.8.3.min.js"></script>
+
+	<script src="js/rga.MicroEvent.js"></script>
+	<script src="js/take2.SlideShow.js"></script>
+	<script src="js/take2.App.js"></script>
+
+	<script src="js/script.js"></script>
+</body>
+</html>
